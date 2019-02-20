45b1a4efa7c818249850685ae3a561b43bff0699
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-02-20 08:34:13 -0700

    Create the Project Archive Page for the custom post type

--- archive-project.php
+++ archive-project.php
@@ -6,7 +6,6 @@
  *
  * @package StanleyWP
  */
-
 get_header(); ?>
 
 	<div class="container">
@@ -26,14 +25,12 @@ get_header(); ?>
 					<?php
 					/* Start the Loop */
 					while ( have_posts() ) : the_post();
-
 						/*
 						 * Include the Post-Format-specific template for the content.
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
 						get_template_part( 'template-parts/content', 'projects' );
-
 					endwhile; ?>
 
 					<div class="col-md-12">
@@ -43,9 +40,7 @@ get_header(); ?>
 					</div><!--  .col-md-12 -->
 
 				<?php else :
-
 					get_template_part( 'template-parts/content', 'none' );
-
 				endif; ?>
 
 				</main><!-- #main -->
--- template-parts/content-projects.php
+++ template-parts/content-projects.php
@@ -6,7 +6,6 @@
  *
  * @package StanleyWP
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-4'); ?>>
